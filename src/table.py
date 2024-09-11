33a1cadeb795b857ab5ae2e67993f714ca1449db
L-Amos <luke.amos77@outlook.com>
2024-09-11 12:34:33 +0100

    Adding stadium_calc function.

--- src/table.py
+++ src/table.py
@@ -111,8 +111,91 @@ class Table:
                 self.phase_space = [phase_space_x, phase_space_y]
             self.collisions = [collisions_x, collisions_y]
 
-    def stadium_calc(self, ball):
+    def stadium_calc(self, ball, phase=True):
+        central_width, central_height = self.dims
+        end_radius = central_height/2
         
+        # Setting things up
+        collisions_x = [ball.pos[0]]  # This and below line needed to plot the start point
+        collisions_y = [ball.pos[1]]
+        arc_length = []
+        cos_angle = []
+        max_t = central_width + central_height  # Maximum amount of time it would take for a collision to occur
+        t = np.arange(0, 1.1*max_t, 1e-3)
+        for i in range(self.reflections):
+            # Work out future x and y values
+            x_test = ball.pos[0] + ball.vel[0]*t
+            y_test = ball.pos[1] + ball.vel[1]*t
+            
+            # Calculate collisions with the top and bottom
+            tb_coll = np.where(y_test < -central_height/2)[0]  # See if anything is below the bottom
+            if not np.any(tb_coll):  # If not bottom, try top
+                tb_coll = np.where(y_test > central_height/2)[0]
+            if not np.any(tb_coll):  # If not, no collision with top or bottom
+                tb_coll = [len(x_test)]  # Set to index higher than any legitimate collision index
+            tb_coll = tb_coll[0]  # First time out of boundary = first time colliding with boundary in this near-continuous case
+            
+            # Calculate collisions with the ends
+            f_left = np.round(x_test+central_width/2 + np.sqrt(np.abs(end_radius**2 - y_test**2)), 2)[1:]  # Function of right-end semicircular boundary
+            f_right = np.round(x_test-central_width/2 - np.sqrt(np.abs(end_radius**2 - y_test**2)), 2)[1:]  # Function of left-end semicircular boundary
+            left_right_end = -1  # Used when calculating normal and tangent vectors
+            end_coll = np.where(f_right>=0)[0]  # See if anything is beyond the right-end boundary
+            if not np.any(end_coll):  # If not right, try left
+                end_coll = np.where(f_left<=0)[0]
+                left_right_end = 1  # Assume that if there's nothing hitting the right, then it must hit the left
+            if not np.any(end_coll):  # If not, no collision with ends
+                end_coll = [len(x_test)]
+            end_coll = end_coll[0]
+            
+            # Find the point of collision with boundary
+            coll_index = min(tb_coll, end_coll)  # Pick whatever we collided with first
+            collision = [x_test[coll_index], y_test[coll_index]]
+            if coll_index == tb_coll:  # If collided with top or bottom, reverse y velocity
+                in_circle = False  # Used later on for phase space calculations
+                ball.vel[1]=-ball.vel[1]
+                tang_vec = [1, 0]
+            else:  # Otherwise, calculate normal/tangent vectors and use those to change velocity
+                in_circle = True
+                diff_x = 2*(collision[0]+(central_width/2)*left_right_end)
+                diff_y = 2*collision[1]
+                norm_vec = [diff_x, diff_y]/np.sqrt(diff_x**2 + diff_y**2)  # Normal unit vector
+                tang_vec = [-diff_y, diff_x]/np.sqrt(diff_x**2 + diff_y**2)  # Tangent unit vector
+                ball.vel = -1*np.dot(ball.vel, norm_vec)*norm_vec + np.dot(ball.vel, tang_vec)*tang_vec
+            ball.pos = collision  # Update ball's position
+            collisions_x.append(ball.pos[0])
+            collisions_y.append(ball.pos[1])
+            
+            if phase:
+                cos = np.dot(ball.vel, tang_vec)  # Get cosine of angle trajectory makes with tangent
+                cos_angle.append(cos)  # y-axis of phase space plot
+
+                # Arc Length Calculation
+                box_perimeter = 2*central_width
+                semicircle_perimeter = np.pi*central_height/2
+                s = 0
+                if in_circle:
+                    if ball.pos[0] > 1:  # If in right circle
+                        angle = np.arctan(ball.pos[1]/(ball.pos[0]-central_width/2))
+                        if ball.pos[1] <= 0:  # If below center of circle
+                            angle += np.pi/2
+                            s += box_perimeter + 3/2 * semicircle_perimeter
+                        s += end_radius * angle
+                    else:  # If in left circle
+                        s += box_perimeter/2 + semicircle_perimeter/2
+                        angle = np.arctan(ball.pos[1]/(ball.pos[0]+central_width/2)) - np.pi/2
+                        if round(angle, 3) <= 0:
+                            angle += np.pi
+                        s += end_radius * angle
+                else:
+                    if ball.pos[1] > 0:  # If colliding with top
+                        s+= semicircle_perimeter/2 + central_width/2 - ball.pos[0]
+                    else:  # Otherwise, colliding with bottom
+                        s+= semicircle_perimeter * 3/2 + central_width + ball.pos[0] + central_width/2
+                arc_length.append(s)  # x-axis of phase space plot
+        if phase:
+            self.phase = [arc_length, cos_angle]
+        self.collisions = [collisions_x, collisions_y]
+
 
     def plot(self, ball, animate=True):
         if self.phase_space:
