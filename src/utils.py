5a977b29bcaadfe8c9a25ff573cdd47e4e064017
Luke Amos <luke.amos.24@ucl.ac.uk>
2024-12-09 18:35:21 +0000

    Adding/updating docstrings.

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,22 @@ import sys
 import numpy as np
 
 def input_test(question, integer=True, positive=False):
+    """Asks user for input and then tests inputs are of the correct type and form.
+
+    Parameters
+    ----------
+    question : str 
+        Question to ask user to receive input.
+    integer : bool, optional
+        Flag for whether input needs to be an integer, by default True.
+    positive : bool, optional
+        Flag for whether input needs to be positive, by default False.
+
+    Returns
+    -------
+    any
+        Input of the correct type and form.
+    """
     while True:
         user_input = input(question)
         if user_input.lower() == "q":
@@ -24,47 +40,47 @@ def input_test(question, integer=True, positive=False):
     return user_input
 
 def update(num, collisions_x, collisions_y, line, start_pos):
-    """Plot Update Function
-    
-    Animates plotting of trajectories.
+    """Animates plotting of trajectories.
     
     Parameters
     ----------
         num: int
-            serves as an incrementer
+            Serves as an incrementer.
         collisions_x: 1D array
-            x coordinates of collision points
+            x coordinates of collision points.
         collisions_y: 1D array
-            y coordinates of collision points
+            y coordinates of collision points.
         line: matplotlib object
-            line of the matplotlib plot
+            Line of the matplotlib plot.
+        start_pos: matplotlib object
+            Scatter point representing the starting position of the ball.
     
     Returns
     -------
         line: matplotlib object
-            updated line for matplotlib plot
+            Updated line for matplotlib plot.
+        start_pos: matplotlib object
+            Scatter point representing the starting position of the ball.
     """
     line.set_data(collisions_x[:num+1], collisions_y[:num+1])
     return line, start_pos
 
 def arc_length(angle, a, b):  
-    """Arc Length of Ellipse 
-    
-    Integrated to calculate arc length of the ellipse from right-most point to point of collision.
+    """Integrated to calculate arc length of the ellipse from right-most point to point of collision.
     
     Parameters
     ----------
         angle: float
-            elliptical angle in radians 
+            Elliptical angle in radians.
         a: float
-            semi-major axis of the ellipse
+            Semi-major axis of the ellipse.
         b: float
-           semi-minor axis of the ellipse
+           Semi-minor axis of the ellipse.
     
     Returns
     -------
         f: float
-            integrand of ellpise arc length calculation
+            Integrand of ellpise arc length calculation.
     """
     f = np.sqrt((a*np.cos(angle))**2+(b*np.sin(angle))**2)
     return f
