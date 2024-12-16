26026dfff20ef307591e0214b2aeb0948211b5a0
L-Amos <luke.amos.24@ucl.ac.uk>
2024-12-16 19:38:13 +0000

    Adding checker to display error if any input is invalid.

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import os
 from pathlib import Path
+import numpy as np
 from mathematical_billiards.table import Table
 from mathematical_billiards.ball import Ball
 
@@ -21,6 +22,8 @@ def my_form_post():
     geometry = request.form['geometry']
     width = float(request.form['width'])
     height = float(request.form['height'])
+    if width <= 0 or height <= 0:
+        return render_template("form.html", finished="Error", error_msg="ERROR: width and height must be greater than zero.")
     x = float(request.form['start_x'])
     y = float(request.form['start_y'])
     angle = float(request.form['angle'])
@@ -28,11 +31,20 @@ def my_form_post():
     billiards_table.reflections = 100
     billiards_ball = Ball(billiards_table, x, y, angle)
     if geometry == "rectangle":
-        billiards_table.rectangle_calc(billiards_ball)
+        if abs(x) <= width/2 and abs(y) <= height/2:
+            billiards_table.rectangle_calc(billiards_ball)
+        else:
+            return render_template("form.html", finished="Error", error_msg="ERROR: starting position not on the table.")
     elif geometry == "elliptical":
-        billiards_table.elliptical_calc(billiards_ball)
+        if (x/width)**2 + (y/height)**2 <= 1:
+            billiards_table.elliptical_calc(billiards_ball)
+        else:
+            return render_template("form.html", finished="Error", error_msg="ERROR: starting position not on the table.")
     else:
-        billiards_table.stadium_calc(billiards_ball)
+        if not ((x > width and x-width/2 > np.sqrt((height/2)**2-y**2)) or (x < width/2 and x+width/2 < -np.sqrt((height/2)**2-y**2))):
+            billiards_table.stadium_calc(billiards_ball)
+        else:
+            return render_template("form.html", finished="Error", error_msg="ERROR: starting position not on the table.")
     # Get location to save GIF
     current_folder = Path(__file__).parent.resolve()
     save_loc = current_folder / "static/scatter.gif"
