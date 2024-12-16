8d7f059358303e0460209beadc62f9978c87f704
L-Amos <luke.amos.24@ucl.ac.uk>
2024-12-16 16:21:15 +0000

    First implementation of webapp.

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template, request
-from src.main import main
+from flask import Flask, render_template, request, send_file
+import os
+from src.table import Table
+from src.ball import Ball
 
 app = Flask(__name__)
 
+gif_path = os.path.join('static', 'scatter.gif')
+
 @app.route('/')
 def test():
-    return render_template("form.html")
+    return render_template("form.html", finished="False")
+
+@app.route('/')
+def loading():
+    return render_template("loading.html")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['geometry']
-    processed_text = text.upper()
-    return processed_text
+    geometry = request.form['geometry']
+    width = float(request.form['width'])
+    height = float(request.form['height'])
+    x = float(request.form['start_x'])
+    y = float(request.form['start_y'])
+    angle = float(request.form['angle'])
+    billiards_table = Table(geometry, width, height)
+    billiards_table.reflections = 100
+    billiards_ball = Ball(billiards_table, x, y, angle)
+    if geometry == "rectangle":
+        billiards_table.rectangle_calc(billiards_ball)
+    elif geometry == "elliptical":
+        billiards_table.elliptical_calc(billiards_ball)
+    else:
+        billiards_table.stadium_calc(billiards_ball)
+    billiards_table.plot(billiards_ball, save="static/scatter.gif")
+    return render_template("form.html", finished="True")
